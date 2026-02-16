55e394c44106b31d0cd920437e13e4b39280dbad
uvsq22201695 <113375225+uvsq22201695@users.noreply.github.com>
2026-02-16 22:25:06 +0100

    style(cipher): add [[nodiscard]] to ICipher interface methods

--- src/interfaces/ICipher.h
+++ src/interfaces/ICipher.h
@@ -13,14 +13,14 @@ public:
     virtual ~ICipher() = default;
 
     // Méthode que chaque cipher DOIT implémenter
-    virtual Block encrypt(Block plaintext) const = 0;
-    virtual Block decrypt(Block ciphertext) const = 0;
+    [[nodiscard]] virtual Block encrypt(Block plaintext) const = 0;
+    [[nodiscard]] virtual Block decrypt(Block ciphertext) const = 0;
 
     // Pour savoir si on attaque du 16 bits, 32 bits, etc.
-    virtual int getBlockSize() const = 0;
+    [[nodiscard]] virtual int getBlockSize() const = 0;
     
     // Alias pour compatibilité avec l'analyse différentielle
-    virtual uint32_t block_size_bits() const { return static_cast<uint32_t>(getBlockSize()); }
+    [[nodiscard]] virtual uint32_t block_size_bits() const { return static_cast<uint32_t>(getBlockSize()); }
 };
 
 
